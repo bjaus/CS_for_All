eea1a5251b37a4bdce551c8f236c104a593e28dd
Jaus <brandon.jaus@gmail.com>
2017-02-17 12:40:40 -0600

    Added first names file and created test to see the distance between these names and mine

--- Functional_Programming/02-Recursion/recursion.py
+++ Functional_Programming/02-Recursion/recursion.py
@@ -18,7 +18,7 @@ def subset(capacity, items):
     if capacity <= 0 or items == []:
         return 0
     elif items[0] > capacity:
-        return subset(capacity, [items[1:])
+        return subset(capacity, items[1:])
     else:
         lose_it = subset(capacity, items[1:])
         use_it = items[0] + subset(capacity - items[0], items[1:])
@@ -32,3 +32,28 @@ def distance(first, second):
         return len(second)
     elif second == '':
         return len(first)
+    elif first[0] == second[0]:
+        return distance(first[1:], second[1:])
+    else:
+        substitution = 1 + distance(first[1:], second[1:])
+        deletion = 1 + distance(first[1:], second)
+        insertion = 1 + distance(first, second[1:])
+        return min(substitution, deletion, insertion)
+        
+        
+if __name__ == '__main__':
+
+    from time import sleep
+
+    me = 'Brandon'
+    results = []
+    
+    with open('first_names.txt', 'r') as fin:
+        for name in fin:
+            name = name.strip()
+            results.append((distance(me, name), name))
+            
+    results.sort()
+    with open('first_name_distance.csv', 'w') as fout:
+        for dist, name in results:
+            fout.write('{},{}\n'.format(name, dist))
