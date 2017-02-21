bb846b147998f4472acca2b5f18009873125bce7
Jaus <brandonjaus@remingtonhotels.com>
2017-02-21 05:34:12 -0600

    Wrote map reduce algorithm

---
+++ Functional_Programming/03/mapreduce.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    '''Returns the sum of two arguments.'''
+    return x + y
+    
+    
+def triple(x):
+    '''Takes a number x as an argument.'''
+    return x * 3
+    
+    
+def mapReduce(mapFunction, reduceFunction, myList):
+    '''Applies mapFunction to myList to construct a new list
+    and then applies reduceFunction to the new list
+    and returns that value.'''
+    return reduce(reduceFunction, map(mapFunction, myList))
+    
+    
+    
+if __name__ == '__main__':
+    
