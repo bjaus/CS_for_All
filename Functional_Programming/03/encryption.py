4bb0665044b0c2718f0ce0fe846d0bd595815eae
bjaus <brandon.jaus@gmail.com>
2017-02-21 13:00:11 -0600

    Added resource folder containing additional exercises

--- Functional_Programming/03/encryption.py
+++ Functional_Programming/03/encryption.py
@@ -2,16 +2,21 @@ import random
 from prime import primeSieve
 
 
+def inverse(e, m):
+    '''Returns the inverse of e mod m'''
+    return list(filter(lambda d: e * d % m == 1, list(range(1, m))))[0]
+
+
 def makeEncoderDecoder():
     '''Returns two functions: An RSA encryption function
     and an RSA decryption function.'''
     
     # Choose 2 primes:
     
-    p, q = random.sample(primeSieve(list(range(2, 10)), 2))
+    p, q = random.sample(primeSieve(list(range(2, 10))), 2)
     n = p * q # compute n
     m = (p - 1) * (q - 1) # compute m
-    print('Maximum number that can be encrypted is ' n - 1)
+    print('Maximum number that can be encrypted is', n - 1)
     
     # Choose a random prime for e
     
@@ -28,4 +33,4 @@ def makeEncoderDecoder():
 
 if __name__ == '__main__':
     # print(primeSieve(list(range(1001))))
-    print(random.sample(list(range(2, 1000)), 10))
+    print(makeEncoderDecoder())
